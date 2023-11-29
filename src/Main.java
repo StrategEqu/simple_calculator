65f9879f628ae9deb562002a328bc2bf18c3d5a2
Strategy <Dima.kogtev.95@mail.ru>
2023-11-29 03:25:11 +0300

    last

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,62 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("Выберите операцию:");
+        System.out.println("1.Вычитание");
+        System.out.println("2.Сложение");
+        System.out.println("3.Умножение");
+        System.out.println("4.Деление");
+        System.out.println("5.Выход");
+        Scanner scanner = new Scanner(System.in);
+        int init = scanner.nextInt();
+        int result;
+        if (init == 5) {
+            System.out.println("Выходим");
+        } else if (init == 1) {
+            System.out.println("Введите первое число:");
+            int first = scanner.nextInt();
+            System.out.println("Введите второе число:");
+            int second = scanner.nextInt();
+            System.out.println("a: " + first);
+            System.out.println("b: " + second);
+            System.out.println("Результат вычитания:");
+            result = first - second;
+            System.out.println("Ответ: " + result);
+        } else if (init == 2) {
+            System.out.println("Введите первое число:");
+            int first = scanner.nextInt();
+            System.out.println("Введите второе число:");
+            int second = scanner.nextInt();
+            System.out.println("a: " + first);
+            System.out.println("b: " + second);
+            System.out.println("Результат сложения:");
+            result = first + second;
+            System.out.println("Ответ: " + result);
+        } else if (init == 3) {
+            System.out.println("Введите первое число:");
+            int first = scanner.nextInt();
+            System.out.println("Введите второе число:");
+            int second = scanner.nextInt();
+            System.out.println("a: " + first);
+            System.out.println("b: " + second);
+            System.out.println("Результат умножения:");
+            result = first * second;
+            System.out.println("Ответ: " + result);
+        } else if (init == 4) {
+            System.out.println("Введите первое число:");
+            int first = scanner.nextInt();
+            System.out.println("Введите второе число:");
+            int second = scanner.nextInt();
+            System.out.println("a: " + first);
+            System.out.println("b: " + second);
+            System.out.println("Результат деления:");
+            if (second == 0) {
+                System.out.println("На ноль делить нельзя");
+            }
+            result = first / second;
+            System.out.println("Ответ: " + result);
         }
+        System.out.println("Программа завершена");
     }
 }
